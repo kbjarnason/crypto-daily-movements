2862d80a9f0695f254393566d4a37f67e1677263
DinkoJuric <dinkovalentino@gmail.com>
2020-05-31 18:35:51 +0200

    Remove first row

--- predictions.jl
+++ predictions.jl
@@ -1,1 +1,5 @@
+using Pkg, Revise, DataFrames, CSV, LinearAlgebra, Dates, Statistics, MLJ, MLJBase, MLJModels, MLJLinearModels, Plots, Flux, MLBase, StatsBase, ExcelReaders
 
+CSV.read("C:\\Users\\dinko\\github\\crypto-daily-movements\\Coinbase_BTCUSD_d.csv")
+
+readxlsheet("Coinbase_BTCUSD_d.csv", "Sheet1", skipstartrow=1)
