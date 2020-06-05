8766fec0afe5241b560bb497d1bb166096f7f457
kbjarnason <kristian.bjarnason@gmail.com>
2020-06-05 18:49:32 +0200

    minor changes

--- predictions.jl
+++ predictions.jl
@@ -13,8 +13,20 @@ Multiple models stacked?
 maybe split to only predict last 100 days for speed?
 lagged variables
 NN (flux, TF)
+
 #%%
 using Pkg, Revise, LinearAlgebra, DataFrames, CSV, Impute, Dates, Statistics, StatsBase, MLJ, MLJBase, MLJModels, MLJLinearModels, Plots
+
+#%%md
+Constants
+#%%
+WINDOW_SIZE = 100
+
+#%%md
+Functions
+#%%
+
+
 #%%
 BTC = CSV.read("Coinbase_BTCUSD_d.csv")
 ETH = CSV.read("Coinbase_ETHUSD_d.csv")
@@ -22,6 +34,7 @@ LTC = CSV.read("Coinbase_LTCUSD_d.csv")
 
 #%%
 cryptos = [BTC, ETH, LTC]
+crypto_names = ["BTC", "ETH", "LTC"]
 
 for (x,item) in enumerate(cryptos)
     item.LR =  append!([0.0], log.(item.Close[2:end]) - log.(item.Close[1:end-1]))
